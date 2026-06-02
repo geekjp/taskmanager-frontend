58335c33a800480936bd5fa6388be676b74e78b1
Jatin Panchal <jatinpanchal0906@gmail.com>
2026-06-02 23:23:37 +0530

    Prepare frontend for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
-/*
------------------------------------
-Axios interceptor
-Runs before every request
------------------------------------
-*/
 api.interceptors.request.use((config) => {
-
-  // Get stored JWT token
   const token = localStorage.getItem("token");
 
-  // If token exists → attach to headers
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
